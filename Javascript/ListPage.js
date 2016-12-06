d2906d4199c5a7a50867bdaa4fc9367802794fd6
KoenduBuf <koen.du.buf@gmail.com>
2016-12-06 17:43:55 +0100

    list page adjustments

--- Javascript/ListPage.js
+++ Javascript/ListPage.js
@@ -1,7 +1,33 @@
-function changeText2(){
-	var itemNameInput = document.getElementById('itemNameInput').value;
-	var node=document.createElement("LI");
-	var textnode=document.createTextNode(itemNameInput);
-	node.appendChild(textnode);
-	document.getElementById("list1").appendChild(node);
+
+
+function addItemButton(){
+	var newItem=new ToDoItem(document.getElementById("itemNameInput").value,3,12,true);
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(newItem.name));
+	addToListWithID(li,"list1");
 }
+
+
+
+
+
+function ToDoItem(name, priority, dueDate, reminder){
+	this.name=name;
+	this.priority=priority;
+	this.dueDate=dueDate;
+	this.reminder=reminder;
+}
+
+function addToListWithID(li, idValue){
+	var ul=document.getElementById(idValue);
+	ul.appendChild(li);
+}
+function addToListWithClass(li, classValue){
+	var ul=document.getElementsByClassName(classValue);
+	ul.appendChild(li);
+}
+
+
+
+
+disList= new ItemList("disList");
