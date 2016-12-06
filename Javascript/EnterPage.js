0af70664641d0de78fb2394719a324058bd43f1e
KoenduBuf <koen.du.buf@gmail.com>
2016-12-06 23:22:36 +0100

    a few changes made some pages and le buttons work

--- Javascript/EnterPage.js
+++ Javascript/EnterPage.js
@@ -1,8 +1,21 @@
-function mainHeaderResize(){
-	mainHeader.innerText="test";
+function singUp() {
+	var emailValue=document.getElementById("email").value;
+	var passValue=document.getElementById("password").value;
+	
+	if(emailValue && passValue){
+		//When both fields are filled
+		
+		window.location="ToDoodleListPage.html";
+	}
 }
 
-
-function singUp() {
+function login(){
+	var emailValue=document.getElementById("email").value;
+	var passValue=document.getElementById("password").value;
 	
+	if(emailValue && passValue){
+		//When both fields are filled
+		
+		window.location="ToDoodleListPage.html";
+	}
 }
--- Javascript/ListPage.js
+++ Javascript/ListPage.js
@@ -1,33 +1,118 @@
-
+/*	  Controllers	*/
 
 function addItemButton(){
-	var newItem=new ToDoItem(document.getElementById("itemNameInput").value,3,12,true);
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(newItem.name));
-	addToListWithID(li,"list1");
+	var inputName=document.getElementById("itemNameInput").value;
+	var inputDueDate=document.getElementById("dueDateInput").value;
+	var inputPriority=document.getElementById("selectPriorityID").value;
+	var inputReminder=document.getElementById("reminderCheckbox").checked;
+	
+	if(inputName && inputDueDate && inputPriority){
+		var newItem=new ToDoItem(inputName,inputPriority,inputDueDate,inputReminder,false);
+		if(!TheList.contains(newItem)){
+			TheList.addItem(newItem);
+			addToDoItemToTable(newItem, "tableList");
+		}
+		
+	}
 }
 
 
 
+/*	  /Controllers	*/
+/*		Classes		*/
 
 
-function ToDoItem(name, priority, dueDate, reminder){
+
+
+function ToDoItemList(ListName){
+	this.name=ListName;
+	this.list=[];
+	this.counter=0;
+	this.addItem=function(Item){
+		this.list[this.counter]=Item;
+		this.counter=this.counter+1;
+	}
+	this.contains=function(Item){
+		var i=this.counter-1;
+		while(i>=0){
+			if(Item.equals(this.list[i])){
+				return true;
+			}
+			i--;
+		}
+		return false;
+	}
+}
+
+
+function ToDoItem(name, priority, dueDate, reminder, done){
 	this.name=name;
 	this.priority=priority;
 	this.dueDate=dueDate;
 	this.reminder=reminder;
+	this.done=done;
+	this.equals=function(Other){
+		if(this.name != Other.name){
+			return false;
+		}
+		if(this.priority != Other.priority){
+			return false;
+		}
+		if(this.dueDate != Other.dueDate){
+			return false;
+		}
+		if(this.reminder != Other.reminder){
+			return false;
+		}
+		if(this.done != Other.done){
+			return false;
+		}
+		return true;
+	}
 }
 
-function addToListWithID(li, idValue){
-	var ul=document.getElementById(idValue);
-	ul.appendChild(li);
+
+
+/*		/Classes	*/
+/*		Methods		*/
+
+
+
+function addToDoItemToTable(ToDoI, idTableValue){
+	var table=document.getElementById(idTableValue);
+	var row=table.insertRow(1);
+	var cell1=row.insertCell(0);
+	var cell2=row.insertCell(1);
+	var cell3=row.insertCell(2);
+	var cell4=row.insertCell(3);
+	var cell5=row.insertCell(4);
+	cell1.innerHTML=ToDoI.name;
+	cell2.innerHTML=ToDoI.dueDate;
+	if(ToDoI.priority=="High"){
+		cell3.className="red";
+	}else if(ToDoI.priority=="Medium"){
+		cell3.className="orange";
+	}else if(ToDoI.priority=="Low"){
+		cell3.className="green";
+	}
+	cell3.innerHTML=ToDoI.priority;
+	if(ToDoI.reminder){
+		cell4.className="green";
+	}else{
+		cell4.className="red";
+	}
+	cell4.innerHTML=ToDoI.reminder;
+	if(ToDoI.done){
+		cell5.className="green";
+	}else{
+		cell5.className="red";
+	}
+	cell5.innerHTML=ToDoI.done;
 }
-function addToListWithClass(li, classValue){
-	var ul=document.getElementsByClassName(classValue);
-	ul.appendChild(li);
-}
 
 
+/*		/Methods	*/
+/*		Fields		*/
+TheList=new ToDoItemList("YourList");
 
-
-disList= new ItemList("disList");
+/*		/Fields		*/
