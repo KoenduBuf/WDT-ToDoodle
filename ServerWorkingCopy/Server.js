d6511f89ee313b24d2dfe1363e9e1c3f8bfc2a44
webdata <koen.du.buf@gmail.com>
2017-01-12 16:24:59 +0100

    made stuff

--- ServerWorkingCopy/Server.js
+++ ServerWorkingCopy/Server.js
@@ -3,16 +3,38 @@ var express = require('express');
 var url = require('url');
 var http = require('http');
 var fs = require('fs');
+var mysql = require("mysql");
+var util = require('util');
 
 
-//The list!
-var todos=[];
-todos=readObject("./list.json");
+//Connected to database
+var conToDb = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "webdata",
+	database: "todo"
+});
+conToDb.connect(function(err){
+	if(err){
+		console.log("Error when connecting to the database");
+		return;
+	}
+	console.log("Connection to database established");
+});
 
 
 
 
-		//Starting da server
+//Old: The list!
+//var todos=[];
+//todos=readObject("./list.json");
+
+
+
+
+
+
+//Starting da server
 var app = express();
 app.use(express.static(__dirname + "/client"));
 //specify port:
@@ -20,7 +42,7 @@ var port=2345;
 //create server app on specified port
 http.createServer(app).listen(port);
 //print to console that server started
-console.log("[i] - Server listening on port "+ port);
+console.log("[i] - Server listening on port " + port);
 
 
 
@@ -33,35 +55,43 @@ console.log("[i] - Server listening on port "+ port);
 
 //When client requests todos:
 app.get("/todos", function (req, res){
-//	console.log("[i] /todos");
 	//send back todos in json format
-	res.json(todos);
+	var dataThing = [];
+	conToDb.query('SELECT Id, Title, Completed, Priority FROM ToDoItem',function(err, rows){
+		if(err) throw err;
+		for (var i = 0; i< rows.length;i++){
+			dataThing.push(rows[i]);
+		}
+		console.log(dataThing);
+	});
+
+	
+	//res.end(JSON.stringify(data));
 });
 
 //When client adds todo to server:
 app.get("/additem", function (req, res) {
 //	console.log("[i] /additem");
 	//make query from url
-	var url_parts=url.parse(req.url, true);
+	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
-	var newId=todos.length+1;
+	//var newId = conToDb.query('SELECT MAX(Id) FROM ToDoItem') + 1;
+
 	
-	//add item based on query
-	if(query["name"]!==undefined) {
-		var newItem={ name : query["name"],
-			id : newId,
-			priority : query["priority"],
-			dueDate : query["dueDate"],
-			reminder : query["reminder"],
-			done : false
+	//add item based on query 
+	if(query["Title"]!==undefined) {
+		var newItem={ Title : query["Title"],
+			Priority : query["Priority"],
+			//DueDate : query["DueDate"],
+			Completed : query["Completed"]
 		};
-		todos.push(newItem);
-		writeObject("./list.json",todos);
-		console.log("[u] >>> added item "+newItem.id);
-		res.json(todos);
+		conToDb.query('INSERT INTO ToDoItem SET ?', newItem, function(err, res){
+			if(err) throw err;
+			console.log("Last added ID: " + res.insertId)
+		});
 	}else{
 		//if name is not defined, send back error
-		res.end("Error: missing name");
+		res.end("Error: missing Title");
 	}
 });
 //When the client deletes something from the todos
@@ -71,16 +101,15 @@ app.get("/deleteitem", function (req, res) {
 	var url_parts=url.parse(req.url, true);
 	var query = url_parts.query;
 
-	if(query["id"]!==undefined) {
-		todos=todos.filter(function(e){
-			return e.id != query["id"];
+	if(query["Id"]!==undefined) {
+		conToDb.query('DELETE FROM ToDoItem WHERE Id=?', query["Id"],function(err, res){
+			if(err) throw err;
+			console.log("Deleted: " + res.affectedRows + " rows");
 		});
-		writeObject("./list.json",todos);
-		console.log("[u] >>> deleted "+query["id"]);
 		res.redirect("/html/ToDoodleListPage.html");
 	}else{
 		//if name is not defined, send back error
-		res.end("Error: missing id");
+		res.end("Error: missing Id");
 	}
 });
 //Modify a todo item
@@ -130,38 +159,26 @@ app.get("/modifyitem", function (req, res) {
 
 
 
-function ToDoItem(name, priority, dueDate, reminder, done, id){
-	this.name=name;
-	this.priority=priority;
-	this.dueDate=dueDate;
-	this.reminder=reminder;
-	this.done=done;
-	this.id=id;
-}
-
-
-
-
-
-
-
-function writeObject(fileName, object){
+//function ToDoItem(name, priority, dueDate, reminder, done, id){
+//	this.name=name;
+//	this.priority=priority;
+//	this.dueDate=dueDate;
+//	this.reminder=reminder;
+//	this.done=done;
+//	this.id=id;
+//}
+//function writeObject(fileName, object){
 	//Open up a write stream to the file
-	var writeStream = fs.createWriteStream(fileName);
+//	var writeStream = fs.createWriteStream(fileName);
 	//write the object to the file in json format (does not do functions)	
 //	console.log("[d] written: "+fileName);
-	writeStream.write(JSON.stringify(object));
-}
-
-function readObject(fileName){
+//	writeStream.write(JSON.stringify(object));
+//}
+//function readObject(fileName){
 	//Open up a read stream to the file the object is in
-	var content=fs.readFileSync(fileName);
+//	var content=fs.readFileSync(fileName);
 	//puts the content in an object
 //	console.log("[d] read: "+fileName);
-	var object=JSON.parse(content);
-	return object;
-}
-
-
-
-
+//	var object=JSON.parse(content);
+//	return object;
+//}
--- ServerWorkingCopy/client/js/ListPage.js
+++ ServerWorkingCopy/client/js/ListPage.js
@@ -29,13 +29,11 @@ var refreshList = function(){
 
 //The function that is called when the add button is pressed
 function addItemButton(){
-	var inputName=document.getElementById("itemNameInput").value;
-	var inputDueDate=document.getElementById("dueDateInput").value;
+	var inputTitle=document.getElementById("itemNameInput").value;
 	var inputPriority=document.getElementById("selectPriorityID").value;
-	var inputReminder=document.getElementById("reminderCheckbox").checked;
 
 	if(inputName){
-		window.location="../additem?name="+inputName+"&dueDate="+inputDueDate+"&priority="+inputPriority+"&reminder="+inputReminder;
+		window.location="../additem?Title="+inputTitle+"&Priority="+inputPriority+"&Completed=0";
 	}
 }
 
@@ -74,43 +72,43 @@ function addToDoItemToTable(ToDoI, idTableValue){
 
 	//function:
 	//get current date in yyyy-mm-dd format	
-	var getCurrentDate = function () {
-		var currentDate = new Date();
-		var month = currentDate.getMonth() + 1;
-		return currentDate.getFullYear() + "-" + month  + "-" + currentDate.getDate();
-	}
+	//var getCurrentDate = function () {
+	//	var currentDate = new Date();
+	//	var month = currentDate.getMonth() + 1;
+	//	return currentDate.getFullYear() + "-" + month  + "-" + currentDate.getDate();
+	//}
 	//function:
 	//determine which date is earlier
-	var smallerDate = function(dueDate, today) {
-		var resDue = dueDate.split("-");
-		var resToday = today.split("-");
-		if (resToday[2].charAt[1] == null) {
-			resToday[2] = "0" + resToday[2];
-		}
-		if (resDue[0] < resToday[0]) {
-			return true;
-		} else if (resDue[1] < resToday[1]){
-			return true;
-		} else if (resDue[2] < resToday[2]) {
-			return true;
-		} else {
-			return false;
-		}		
-	}
+	//var smallerDate = function(dueDate, today) {
+	//	var resDue = dueDate.split("-");
+	//	var resToday = today.split("-");
+	//	if (resToday[2].charAt[1] == null) {
+	//		resToday[2] = "0" + resToday[2];
+	//	}
+	//	if (resDue[0] < resToday[0]) {
+	//		return true;
+	//	} else if (resDue[1] < resToday[1]){
+	//		return true;
+	//	} else if (resDue[2] < resToday[2]) {
+	//		return true;
+	//	} else {
+	//		return false;
+	//	}		
+	//}
 	
 	//create a button
 	var doneButton = document.createElement("button");
 	var t = document.createTextNode("Remove Item");
 	doneButton.onclick=function(){		
-		window.location="../deleteitem?id="+ToDoI.id;
+		window.location="../deleteitem?id="+ToDoI.Id;
 	}
 	doneButton.appendChild(t);
 	
 	var editButton = document.createElement("BUTTON");
 	var t2 = document.createTextNode("Edit Item");
 	editButton.onclick=function() {
-		var newName=prompt("What would you like as new name?");
-		window.location="../modifyitem?id="+ToDoI.id+"&name="+newName;		
+		var newName=prompt("What would you like as new title?");
+		window.location="../modifyitem?id="+ToDoI.Id+"&Title="+newName;		
 	}
 	editButton.appendChild(t2);
 
@@ -122,30 +120,30 @@ function addToDoItemToTable(ToDoI, idTableValue){
 
 	//fill up cells 1 to 5 with proper 
 	//names and give them classes for colors
-	cell1.innerHTML=ToDoI.name;
-	cell2.innerHTML=ToDoI.dueDate;
-	if(ToDoI.priority=="High"){
-		cell3.className="red";
-	}else if(ToDoI.priority=="Medium"){
-		cell3.className="orange";
-	}else if(ToDoI.priority=="Low"){
-		cell3.className="green";
-	}
-	cell3.innerHTML=ToDoI.priority;
+	cell1.innerHTML=ToDoI.Title;
+	//cell2.innerHTML=ToDoI.DueDate;
+	//if(ToDoI.priority=="High"){
+	//	cell3.className="red";
+	//}else if(ToDoI.priority=="Medium"){
+	//	cell3.className="orange";
+	//}else if(ToDoI.priority=="Low"){
+	//	cell3.className="green";
+	//}
+	cell3.innerHTML=ToDoI.Priority;
 	//if(ToDoI.reminder){
 	//	cell4.className="green";
 	//}else{
 	//	cell4.className="red";
 	//}
-	cell4.innerHTML=ToDoI.reminder;
+	//cell4.innerHTML=ToDoI.reminder;
 	//if(ToDoI.done){
 	//	cell5.className="green";
 	//}else{
 	//	cell5.className="red";
 	//}
-	if(smallerDate(ToDoI.dueDate, getCurrentDate())) {	
-		cell2.className="red";
-	}
+	//if(smallerDate(ToDoI.dueDate, getCurrentDate())) {	
+	//	cell2.className="red";
+	//}
 	//cell5.innerHTML=ToDoI.done;
 	cell6.appendChild(doneButton);
 	cell7.appendChild(editButton);
